61462780816c2f625306faa1377f2de538ca8922
Athisankar-2006 <athisankar2k6@gmail.com>
2025-09-03 18:11:43 +0530

    right triangle 0 and 1 number pattern added

--- patterns/right triangle.py
+++ patterns/right triangle.py
@@ -8,6 +8,30 @@ def right_triangle(n):
         print("")
     return "pattern printed"
 
+
+
+def right_triangle2(n):
+    for i in range(n):
+        for j  in range(i+1):
+            print(j+1,end="")
+        print("")
+    return "pattern printed"
+
+def right_triangle3(n):
+    for i in range(1,n+1):
+        val=1 if i % 2 != 0 else 0
+        for j in range(1,i+1):
+            print(val,end="")
+            val=1-val
+        print("")
+        
+    return "pattern printed"
+
+
+
+
 n=[3,5,10]
 for x in n:
-   print(right_triangle(x))
+    print(right_triangle(x))
+    print(right_triangle2(x))
+    print(right_triangle3(x))
--- patterns/static.py
+++ patterns/static.py
@@ -3,10 +3,21 @@
 def static_pattern(n):
     for i in range(n):
         for j in range(n):
+            print(j+1,end="")
+        print("")
+    return "pattern printed"
+
+def static_pattern2(n):
+    for i in range(n):
+        for j in range(n):
             print("*",end="")
         print("")
     return "pattern printed"
 
-n=[3,5,10]
+
+n=[5]
 for x in n:
    print(static_pattern(x))
+   print(static_pattern2(x))
+
+
