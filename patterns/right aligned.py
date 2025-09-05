81075ea20b195bd2bcf6d91c8c50afddb64a480e
Athisankar-2006 <athisankar2k6@gmail.com>
2025-09-05 22:15:32 +0530

    right aligned patternn added

---
+++ patterns/right aligned.py
@@ -0,0 +1,14 @@
+### 
+
+def pattern(n):
+    m=(n*2)-1
+    for i in  range(m):
+        if (i>n):
+            i=n*2-i
+        for j in range(i):
+            print("*",end="")
+
+        print("")
+    return "pattern printed"
+
+print(pattern(5))
